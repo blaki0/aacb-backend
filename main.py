579ee87dfd9f0eaac23f1d495a3ae429a10b79c9
blaki0 <wendaseng2@gmail.com>
2026-07-10 23:24:09 +0300

    Refactor FastAPI app and database initialization

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Addis Ababa City Bus Garage Management System - Cloud Versi
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
